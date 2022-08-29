625b48c95660ac538abe1be7f41bb69378663257
Tich53 <rdouette@yahoo.fr>
2022-08-29 10:57:34 +0200

    Challenge 3 - Creating a route with parameters + requirements + method GET

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Exception;
+use Doctrine\ORM\EntityNotFoundException;
+use function PHPUnit\Framework\throwException;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/program', name: 'program_')]
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', []);
+    }
+
+    #[Route('/{id<\d+>}', methods: ["GET"], name: 'app_index')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id
+        ]);
     }
 }
